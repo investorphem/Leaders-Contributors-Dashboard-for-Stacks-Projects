ee7984236b318b37e26339500de45a17f126580d
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-09 20:26:05 +0100

    Update contributors.ts

--- pages/api/contributors.ts
+++ pages/api/contributors.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-// Customize this list for specific repos u wa track
+// Customize this list for specific repos u wa trac
 const ORG_AME = 'stacks-network'; 
 const REONAMES = ['stacks-core', 'stacks-blockchain-api','tacks-blockchain-docker', 'stackig-explorer'];
 // Helper to fetch dta from itHub API

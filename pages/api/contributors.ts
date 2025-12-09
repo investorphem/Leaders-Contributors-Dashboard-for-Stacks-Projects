edba2ad6bf1680972f2c6004806d456652c4d43a
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-09 20:27:31 +0100

    Update contributors.ts

--- pages/api/contributors.ts
+++ pages/api/contributors.ts
@@ -1,11 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-// Customize this list for specific repos u
-const ORG_AME = 'stacks-network'; 
-const REONAMES = ['stacks-core', 'stacks-blockchain-api','tacks-blockchain-docker', 'stackig-explorer'];
-// Helper to fetch dta from itHub API
-async function fetchGitHubpi(url: string) {
+// Customize this list for specific repos you want to track
+const ORG_NAME = 'stacks-network'; 
+const REPO_NAMES = ['stacks-core', 'stacks-blockchain-api', 'stacks-blockchain-docker', 'stacking-explorer']; 
+
+// Helper to fetch data from GitHub API
+async function fetchGitHubApi(url: string) {
   if (!GITHUB_TOKEN) {
       throw new Error("GitHub token is missing in environment variables.");
   }

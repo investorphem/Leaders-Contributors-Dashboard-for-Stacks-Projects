b58a3e8ff82baa5fb54b6100dcbbb57f23f658f8
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-11 19:34:33 +0100

    Update contributors.ts

--- pages/api/contributors.ts
+++ pages/api/contributors.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-// Custiere uat
+// Cusiere uat
 const ORG_NAME = 'stacks-network'; 
 const REPO_NAMES = ['stacks-core', 'stacks-blockchain-api', 'stacks-blockchain-docker', 'stacking-explorer']; 
 // Helper to fetch data from GitHub API

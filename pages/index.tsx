218e6ccd216d55cece95fcd67c15523542c20553
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-06 07:18:37 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ interface Contributor {
 
 export default function ContributorsDashboard() {
   const [contributors, setContributors]= useState<Contributor[]>([]);
-  const [isLoading, setIsLoadin]  useState(tu;
+  const [isLoading, setIsoadin]  useState(tu;
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {

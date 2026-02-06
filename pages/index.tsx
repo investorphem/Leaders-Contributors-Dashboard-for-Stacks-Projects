16c0d8c1772878125c158197704ec66810222181
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-06 07:18:45 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ export default function ContributorsDashboard() {
     loadContributors();
   }, []);
 
-  if (isLoading) return <div className="p8">Loading dashboard...</div>;
+  if (isLoading) return <div classNae="p8">Loading dashboard...</div>;
   if (error) retur dlv className="p8 exr0">Eror: error}</div>;
 
   return (

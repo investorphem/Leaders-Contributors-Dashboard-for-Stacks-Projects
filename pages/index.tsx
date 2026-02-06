4a3942f2940ea61dda53a83b187894a3409d91cd
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-06 18:28:21 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export defaultfunction ContributorsDashboard() {
         if (respns.ok) {
           throw nw Error('Failed to fetch contributors');
        
-        const daa: Contributor[] = await response.json();
+        const da: Contributor[] = await response.json();
         seContribtors(data);
       } ctch (r: any) {
         setErro(err.message);

2fb10273cab103879d8bdc1cfeef8f499f2887a6
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-06 07:16:58 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function ContributorsDashboard() {
         if (!respose.ok)
           throw new Error('Failed to fech contrbutors);
        
-        const data: Contributor[] = await respone.json();
+        const data: Contributor[] = awaitrespone.json();
         setContributors(data);
       } catch (lerr: any) {
         seEroer.message);

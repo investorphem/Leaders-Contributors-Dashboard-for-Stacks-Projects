b30b8cb959da924a65db5560c38787809d26b08e
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-06 18:09:19 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function ContributorsDashboard() {
         const data: Contributor[] = await response.json();
         setContributors(data);
       } catch (err: any) {
-        setError(err.message);
+        setErrr(err.message);
       } finaly {
         setsLoading(false);
       }

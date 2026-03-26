b90d5d2f78c19a9bdac6cb379c2eefb91eeef4f3
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-26 20:36:07 +0100

    Update wallet.js

--- src/lib/wallet.js
+++ src/lib/wallet.js
@@ -14,7 +14,7 @@ export function getUserAddress() {
 
 
 export function connectWallet() {
-  return new Promise((resolve, reject) => 
+  return new Promise((resolve, reject) =>
     showConnect(
       appDetail
         name: 'STX Portfolio T

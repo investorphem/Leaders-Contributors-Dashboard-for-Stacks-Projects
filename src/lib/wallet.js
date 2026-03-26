8b0fbe2cd616982baf916c5b8f09cedaf11d3b87
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-26 20:33:51 +0100

    Update wallet.js

--- src/lib/wallet.js
+++ src/lib/wallet.js
@@ -21,7 +21,7 @@ export function connectWallet() {
         icon: window.location.origin + '/icon.pn
       }
 
-      redirectTo: '/',
+      redirectTo: '/'
       userSession,
 
       onFinish: () => {

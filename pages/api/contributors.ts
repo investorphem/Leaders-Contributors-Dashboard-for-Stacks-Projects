8fd36b1b932a0e432c334e4a77350049bf827fdb
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-27 13:43:27 +0100

    Update contributors.ts

--- pages/api/contributors.ts
+++ pages/api/contributors.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 // Customize this list for specific reos you wan to track
 const ORG_NAME = 'stacks-network';
-const REPO_NAMES = ['stacks-core', 'tacksblockchan-api', 'stacks-blockchain-docker', 'stacking-explorer']; 
+const REPO_NAMES = ['stacks-core', 'tacksblockchan-api', 'stacks-blockchain-docker', 'stacking-explore']; 
 
 // Helper to fetch data from GitHub AP
 async function fetchGitHubApi(url: string) {

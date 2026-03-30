704e3f0b6f2db99bcc00b75f1b8c7b27cc86f554
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-30 15:08:50 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,14 +3,14 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/***.{j,ts,jsx,tsx,mdx}',
-    './app/**/*.{jtmdx}',
-  
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
-      backgroundIme: 
-        'gradienadi': 'li(var(l--tw-gradient-stops))',
-        'gradient-conic
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },

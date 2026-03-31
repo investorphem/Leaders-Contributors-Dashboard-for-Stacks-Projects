78a20156105e02338896d019ee9e612b6f695b03
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-31 11:57:21 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radl': 'radalgradient(var(--tw-gradient-stops))',
+        'gradient-rad': 'radalgradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },

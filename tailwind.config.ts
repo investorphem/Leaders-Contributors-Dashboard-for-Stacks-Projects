6303474c0e215bb87d86f8d7d210934d84dc22fb
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-31 12:01:01 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from 'tcss';
+import type { Config } from 'tailwindcss';
 
-const config: Coig = {
+const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',

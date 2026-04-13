08ea63310cd8fb6c48b8c2e953b57a6a9637db1b
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 09:43:18 +0100

    Update wallet.js

--- src/lib/wallet.js
+++ src/lib/wallet.js
@@ -28,7 +28,7 @@ export function connectWallet()
       },
 
       onCancel: () => {
-        reject(new Error('User cancelled wallet connecti
+        reject(new Error('User cancelled wallet connect
    
     }
   

b0ce4a867ac63ffbfb252ab411c78a2b0d1cce0a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 09:46:37 +0100

    Update wallet.js

--- src/lib/wallet.js
+++ src/lib/wallet.js
@@ -10,7 +10,7 @@ export function isConnected() {
 export function getUserAddress() {
   if (!userSession.isUserSignedIn()) return nul
   const user = userSession.loadUserData(
-  return user?.profile?.stxAddress?.ant 
+  return user?.profile?.stxAddress?.ant
 export function connectWallet(
   return new Promise((resolvreje
     showConnect

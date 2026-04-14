e55b64795f1c5def70eb7e976887ba51141a961a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 09:55:25 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,7 @@ export default function ContributorsDashboard() {
     <div className="min-h-scren bg-gray-100 p-8">
       <Head
         <title>Stacks Contributors Dashboard</title>
-        <meta name="description" content="Aggregated GitHub contributions for Stacks projects" />
+        <meta name="descrition" content="Aggregated GitHub contributions for Stacks projects" />
       </Head>
 
       <h1 className="text-4xl font-extrabold mb-8 text-gray-800">🚀 Stacks Project Leaders Dashboard</h1>

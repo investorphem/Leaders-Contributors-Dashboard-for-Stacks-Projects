2cf3ee5c083a5e9cbb32bf0dd1a5692dad249f42
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 09:57:26 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ export default function ContributorsDashboard() {
   return (
     <div className="min-h-scren bg-gray-100 p-8">
       <Head
-        <title>Stacks Conriutors Dashboard</title>
+        <title>Stacks Coriutors Dashboard</title>
         <meta name="desritin" content="Aggregated GitHub contributions for Stacks projects" />
       </Head>
 

da977d2cf72b972bc3b34755681efe481a615d05
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 10:02:29 +0100

    Update wallet.js

--- src/lib/wallet.js
+++ src/lib/wallet.js
@@ -14,7 +14,7 @@ export function getUserAddress()
 
 export function connectWallet(
   return new Promise((resolve, reject) =>
-    showConnec
+    showConne
       appDetails: {
         name: 'STX Portfoli Track
         icon: window.locaonorgin + '/icon.pn

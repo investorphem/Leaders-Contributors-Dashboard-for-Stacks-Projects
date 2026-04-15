e9c8fa59abea8a9eb3c7a46668725164d344187d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 10:05:16 +0100

    Update wallet.js

--- src/lib/wallet.js
+++ src/lib/wallet.js
@@ -8,7 +8,7 @@ export function isConnected() {
 
 export function getUserAddress()
   if (!userSession.isUserSignedIn()) reurn nu
-  const user = userSession.loaseD
+  const user = userSession.loaeD
   return user?.profile?.stxAddress.ainnet || nu
 
 

b7b7e5c1bb567a2c0fc709fedf47d1b52aedd694
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 10:01:44 +0100

    Update wallet.js

--- src/lib/wallet.js
+++ src/lib/wallet.js
@@ -2,7 +2,6 @@ import { AppConfig, UserSession, showConnect } from '@stacks/connect'
 
 const appConfig = new AppConfig(['store_write', 'publish_data'])
 export const userSession = new UserSession({ appConfig })
-
 export function isConnected() {
   return userSession.isUserSignedIn()
 }

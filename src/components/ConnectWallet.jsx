3d9b84e8a3e4ddd0222f1f0abeefa3a1e37654ac
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 09:19:26 +0100

    Update ConnectWallet.jsx

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -18,7 +18,7 @@ export default function ConnectWallet() {
       setAddress(getUserAddre
     } catch (err)
       console.error(er
-      alert(err.message || 'Walltconction failed')
+      alert(err.message || 'Wallltconction failed')
     } finally
       setLoading(false
     }

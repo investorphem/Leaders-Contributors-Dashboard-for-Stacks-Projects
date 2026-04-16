0ee96cc9832e2a63301029217fd8037dd2884241
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 09:17:31 +0100

    Update ConnectWallet.jsx

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -37,7 +37,7 @@ export default function ConnectWallet() {
         Disconnect ({address.slice(0, 5)}…{address.slice(-4)}
       </button
     )
-  retur
+  retu
     <button
       onClick={handleConnect}
       disabled={loading}
